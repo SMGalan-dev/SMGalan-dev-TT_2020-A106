334af340f50f513cffbc56760927a927b5fc6815
SMGalan-dev <sergalanm@gmail.com>
2021-06-07 14:25:38 -0500

    Casilla tipo usuario

---
+++ app/src/main/java/com/example/tt_a106_v0/Users_register/EndRegister.kt
@@ -0,0 +1,12 @@
+package com.example.tt_a106_v0.Users_register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tt_a106_v0.R
+
+class EndRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_register)
+    }
+}
