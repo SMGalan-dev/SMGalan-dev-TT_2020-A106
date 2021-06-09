44fbfff2994fa6519f19aae9304c1058cbeaf1aa
SMGalan-dev <sergalanm@gmail.com>
2021-06-09 12:20:16 -0500

    interaccion al menu añadida

---
+++ app/src/main/java/com/example/tt_a106_v0/patient_fragments/DietFragment.kt
@@ -0,0 +1,7 @@
+package com.example.tt_a106_v0.patient_fragments
+
+import androidx.fragment.app.Fragment
+import com.example.tt_a106_v0.R
+
+class DietFragment : Fragment(R.layout.fragment_diet) {
+}
