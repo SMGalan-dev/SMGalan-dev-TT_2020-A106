9e590fa8f81a02c242a557048039eac158416de9
SMGalan-dev <sergalanm@gmail.com>
2021-06-14 17:11:11 -0500

    BotonAgregarMedicina

--- app/src/main/java/com/example/tt_a106_v0/patient_fragments/MedicationFragment.kt
+++ app/src/main/java/com/example/tt_a106_v0/patient_fragments/MedicationFragment.kt
@@ -6,13 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.tt_a106_v0.R
 
 
 class MedicationFragment : Fragment() {
     private lateinit var mView: View
-    private lateinit var mView2: View
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,22 +19,9 @@ class MedicationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         mView=inflater.inflate(R.layout.fragment_medication,container,false)
-        mView2=inflater.inflate(R.layout.fragment_add_medicine,container,false)
         val newMedicine = mView.findViewById<Button>(R.id.newMedicamentBtn)
-        newMedicine.setOnClickListener {
-     /*
-            val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.nav_host_fragment_content_main_activity_patient1, R.layout,fragment_)
-            transaction?.commit()
-
-      */
-            val transaction2 = activity?.supportFragmentManager?.beginTransaction()?.apply {
-                replace(R.id.nav_host_fragment_container, AddMedicineFragment())
-                commit()
-            }
-
-        }
-        return inflater.inflate(R.layout.fragment_medication, container, false)
+        newMedicine.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_Medicamentos_to_addMedicineFragment, null))
+        return mView
     }
 
 }
